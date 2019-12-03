c1c3033c34134658564af215c595cb68850aea75
Alexsander <9252818@mail.ru>
2019-12-03 16:18:27 +0300

    Upd Task3, most humanity...

--- Task3.py
+++ Task3.py
@@ -1,17 +1,34 @@
-string = eval(input())
-stack = []
-error = False
-for i in string:
-    if type(i) is tuple:
-        for e in i:
-            stack.append(e)
-    else:
-        if i <= len(stack):
-            buffer = []
-            for e in range(0, i):
-                buffer.append(stack.pop(0))
-            buffer = str(buffer)
-            print('('+buffer.strip('[,]')+")")
-        else:
+"""
 
-            break
+Чудо-Конвейер
+
+Ввести последовательность объектов Python (кортежей или целых чисел), и сымитировать работу Чудо-Конвейера.
+Если объект — кортеж, это означает, что на вход конвейеру подаются поочерёдно все объекты из этого кортежа.
+Если объект — натуральное число N, это означает, что с выхода конвейера надо снять поочерёдно N объектов,
+объединить их в кортеж и вывести.
+Если с конвейера нельзя снять N объектов, или в последовательности нет больше команд, Чудо-Конвейер немедленно останавливается.
+
+Example:
+Input:
+("QWE",1.1,234),2,(None,7),0,2,(7,7,7),2,(12,),(),3,(5,6),3,100500
+
+Output:
+('QWE', 1.1)
+()
+(234, None)
+(7, 7)
+(7, 7, 12)
+"""
+
+Conveer_input = eval(input())
+queue = []
+for unit in Conveer_input:
+    if type(unit) is tuple:
+        ##Add elements of tuple in queue
+        queue.extend(unit)
+    elif unit <= len(queue):
+        ##Dequeue Elements
+        print(tuple(queue[0:unit]))
+        del(queue[0:unit])
+    else:
+        break
